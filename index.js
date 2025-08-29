83476fefda30b70ea4a7d5c24baf797eff3f4ab8
Denish2410 <ahalparadenish852@gmail.com>
2025-08-29 14:05:21 +0530

    html done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function renderWeatherinfo(data){
 async function fetchWeatherDetails(){
     try{
         let city="goa";
-        
+    
         const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`); 
         const data=await response.json();
         console.log("Weather data:-> " , data);
